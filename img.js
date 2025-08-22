9340e27c1834a7f93d52d5897dcfd908b82c1c2f
CarlosJaco <carlos.jacobouski@escola.pr.gov.br>
2025-08-22 20:47:42 -0300

    Delete img.js

--- img.js
+++
@@ -1,5 +0,0 @@
-// Exemplo de como mudar a cor do carro
-function changeCarColor(color) {
-    const carImage = document.getElementById('carImage');
-    carImage.src = `images/carro-${color}.png`;
-}
